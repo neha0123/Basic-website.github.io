7fbafd76cecf017b518202644307972de80f8e4f
Neha Garg <garg22neha1999@gmail.com>
2020-12-26 17:25:22 +0530

    upload new file

---
+++ css/try.js
@@ -0,0 +1,10 @@
+console.log("neha cute h ");
+function greet(name){
+    console.log(name+'is a good boy')
+}
+let name="Harry";
+let name1="rohan";
+let name2="nik";
+let name3="sagar";
+greet(name);
+console.log(name2);
